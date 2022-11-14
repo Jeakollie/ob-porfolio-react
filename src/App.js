c42547b2cd2192dfc6e44c3002a940195b594b1e
Obadiah V. Jeakollie, Jr <obadiahjeakollie13@gmail.com>
2022-11-14 16:09:51 +0000

    Initial Commits

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+ <>
+
+
+      <div class="py-6">
+        <main>
+          <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+           {/*  <!-- Replace with your content -->
+
+            <!-- Work --> */}
+            <div id="intro" class="bg-[#
+            3EAEAEA] h-auto my-6 py-8 rounded-lg border-4">
+              <div id="avatar" class="flex justify-center py-2">
+                <img class="w-40" src="https://api.multiavatar.com/JB.svg " alt="JB image" />
+              </div>
+              <div id="content" class="prose lg:prose-xl px-2">
+                <h1 class="text-center text-gray-800 text-4xl capitalize font-medium">Obadiah V. Jeakollie, Jr</h1>
+                <small class="text-center block text-md text-gray-800">Digital Marketeer, Teacher &amp; Software Engineer</small>
+                <p class="text-justify text-lg text-gray-800">Hey! I am Obadiah V. Jeakollie, Jr, I am a Graphics Designer, Full-Stack Web Developer, and Marketing & Branding Strategist at Optechsol.</p>
+              </div>
+            </div>
+
+            {/* <!-- Work --> */}
+            <div id="work" class="bg-[#B2B2B2] h-auto my-6 py-8 rounded-lg border-4">
+              <div class="flex justify-center">
+                <h3 class="border-4 w-20  border-dashed px-4 py-4">Work</h3>
+              </div>
+
+              <details class="px-6 py-4">
+                <summary class="font-bold text-2xl text-gray-800">Optechsol</summary>
+                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+                  pavilions, award-winning fireworks and seasonal special events.</p>
+              </details>
+
+              <details class="px-6 py-4">
+                <summary class="font-bold text-2xl text-gray-800">Optimo Store</summary>
+                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+                  pavilions, award-winning fireworks and seasonal special events.</p>
+              </details>
+
+              <details class="px-6 py-4">
+                <summary class="font-bold text-2xl text-gray-800">Optransol</summary>
+                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+                  pavilions, award-winning fireworks and seasonal special events.</p>
+              </details>
+
+            </div>
+             {/* <!-- Education --> */}
+            <div id="education" class="bg-[#
+            3EAEAEA] h-50 my-6 py-8 rounded-lg border-4"></div>
+            {/* <!-- Hobbies --> */}
+            <div id="hobbie" class="bg-[#B2B2B2] h-50 my-6 py-8 rounded-lg border-4"></div>
+          </div>
+          {/* <!-- /End replace --> */}
+      </main>
+      </div>
+ </>
+
+  )
+ } 
+
+export default App;
---
+++ src/components/Work.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Work() {
+    return (
+        <div id="work" class="bg-[#B2B2B2] h-auto my-6 py-8 rounded-lg border-4">
+        <div class="flex justify-center">
+          <h3 class="border-4 w-20  border-dashed px-4 py-4">Work</h3>
+        </div>
+
+        <details class="px-6 py-4">
+          <summary class="font-bold text-2xl text-gray-800">Optechsol</summary>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+            pavilions, award-winning fireworks and seasonal special events.</p>
+        </details>
+
+        <details class="px-6 py-4">
+          <summary class="font-bold text-2xl text-gray-800">Optimo Store</summary>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+            pavilions, award-winning fireworks and seasonal special events.</p>
+        </details>
+
+        <details class="px-6 py-4">
+          <summary class="font-bold text-2xl text-gray-800">Optransol</summary>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
+            pavilions, award-winning fireworks and seasonal special events.</p>
+        </details>
+
+      </div>
+    )
+}
