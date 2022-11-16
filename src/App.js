1208b97e3a4666c5de4e916b2062b126eb3abf7f
Obadiah V. Jeakollie, Jr <obadiahjeakollie13@gmail.com>
2022-11-16 12:24:07 +0000

    Import React Router

--- src/App.js
+++ src/App.js
@@ -1,61 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import AuthorCard from './components/AuthorCard.js';
+import Education from './components/Education.js';
+import Hobbies from './components/Hobbies.js';
+import Work from './components/Work.js';
+
 
 
 function App() {
+  const [isActive, setIsActive] = useState({
+    active: true,
+    name: 'education'
+  });
   return (
  <>
 
-
-      <div class="py-6">
+          <Navbar />
+      <div className="py-6">
         <main>
-          <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
            {/*  <!-- Replace with your content -->
 
             <!-- Work --> */}
-            <div id="intro" class="bg-[#
-            3EAEAEA] h-auto my-6 py-8 rounded-lg border-4">
-              <div id="avatar" class="flex justify-center py-2">
-                <img class="w-40" src="https://api.multiavatar.com/JB.svg " alt="JB image" />
-              </div>
-              <div id="content" class="prose lg:prose-xl px-2">
-                <h1 class="text-center text-gray-800 text-4xl capitalize font-medium">Obadiah V. Jeakollie, Jr</h1>
-                <small class="text-center block text-md text-gray-800">Digital Marketeer, Teacher &amp; Software Engineer</small>
-                <p class="text-justify text-lg text-gray-800">Hey! I am Obadiah V. Jeakollie, Jr, I am a Graphics Designer, Full-Stack Web Developer, and Marketing & Branding Strategist at Optechsol.</p>
-              </div>
-            </div>
-
+           <AuthorCard isActive={isActive} setIsActive={setIsActive}/>
             {/* <!-- Work --> */}
-            <div id="work" class="bg-[#B2B2B2] h-auto my-6 py-8 rounded-lg border-4">
-              <div class="flex justify-center">
-                <h3 class="border-4 w-20  border-dashed px-4 py-4">Work</h3>
-              </div>
-
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">Optechsol</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">Optimo Store</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-              <details class="px-6 py-4">
-                <summary class="font-bold text-2xl text-gray-800">Optransol</summary>
-                <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international
-                  pavilions, award-winning fireworks and seasonal special events.</p>
-              </details>
-
-            </div>
+            {isActive.name === 'education' ? <Education /> : isActive.name === 'hobbies' ? <Hobbies /> : <Work />}
+            
              {/* <!-- Education --> */}
-            <div id="education" class="bg-[#
-            3EAEAEA] h-50 my-6 py-8 rounded-lg border-4"></div>
-            {/* <!-- Hobbies --> */}
-            <div id="hobbie" class="bg-[#B2B2B2] h-50 my-6 py-8 rounded-lg border-4"></div>
           </div>
+            {/* <!-- Hobbies --> */}
+            
           {/* <!-- /End replace --> */}
       </main>
       </div>
